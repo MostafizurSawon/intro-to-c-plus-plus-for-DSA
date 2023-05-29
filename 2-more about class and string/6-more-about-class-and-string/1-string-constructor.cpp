0ea9c67f78f894c66603935c8c981687e7f6ff0b
Mostafizur06 <mostafizur1102@gmail.com>
2023-05-29 12:41:44 +0600

    module 6 done

---
+++ 2-more about class and string/6-more-about-class-and-string/1-string-constructor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // string s="Sawon is my name";
+  // string a="Sawon is my name";
+  string s("Mostafizur", 5);
+  // string s(a,4);
+
+  // string s(7,'m');
+
+
+  cout <<s << endl;
+
+  return 0;
+}
---
+++ 2-more about class and string/6-more-about-class-and-string/6-this-keyword-c++.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Mobile
+{
+  public:
+    string name;
+    int release_year;
+    int ram;
+    int rom;
+    int android_version;
+    string processore;
+
+    Mobile(string name,int rd,int rm,int rmm,int av,string p)
+    {
+      this->name = name;
+      release_year = rd;
+      ram = rm;
+      rom = rmm;
+      android_version = av;
+      processore = p;
+    }
+
+    void print()
+    {
+      cout << "Model Name: " << name << endl;
+      cout << "Release year: " << release_year << endl;
+      cout << "Android Version: " << android_version << endl;
+      cout << "Processor: " << processore << endl;
+      cout << "Ram: " << ram << " GB"<< endl;
+      cout << "Rom: " << rom << " GB"<< endl;
+    }
+
+    int ramrom()
+    {
+      return ram+rom;
+    }
+
+
+};
+
+int main()
+{
+  Mobile s10("Samsung galaxy s10+", 2018, 8, 128, 10, "Snapdragon 855");
+  s10.print();
+  cout<<s10.ramrom();
+
+  return 0;
+}
